27f3642ea798027d6298309a1eb0b5f874b0177d
Tal Zaken <talzaken@gmail.com>
2021-11-09 08:16:53 -0500

    pretty printing results, checked different endpoint.

--- GetStops.py
+++ GetStops.py
@@ -3,12 +3,12 @@ import json
 import pprint
 
 sess = requests.session()
-response = sess.get('https://api-v3.mbta.com/predictions/?filter[stop]=place-chill&filter[route]=Green-B&filter[direction_id]=1')
+response = sess.get('https://api-v3.mbta.com/stops?fields[stop]=name,description')
 
 print(response)
 
 results = response.json()
-print(results['data'])
+pprint.pprint(results['data'])
 
 
 #print(json.dumps(results, indent='\t'))
